a4615308c37a9f2d5bf3c69057182acb7e595244
Ivanenko Oleg <ivanenko.oleg.m@gmail.com>
2017-11-16 07:35:08 +0200

    80%

--- app/Http/Controllers/TspController.php
+++ app/Http/Controllers/TspController.php
@@ -15,33 +15,61 @@ class TspController extends Controller
     private $shortest_route = array();	// holds the shortest route
     private $shortest_routes = array();	// any matching shortest routes
     private $shortest_distance = 0;		// holds the shortest distance
-    private $all_routes = array();		// array of all the possible combinations and there distances
+    private $all_routes = array();		// array of all the  possible combinations and there distances
 
 
     // add a location
-    public function add($name,$longitude,$latitude){
-        $this->locations[$name] = array('longitude'=>$longitude,'latitude'=>$latitude);
-    }
+//    public function add($name,$longitude,$latitude){
+//        $this->locations[$name] = array('longitude'=>$longitude,'latitude'=>$latitude);
+//        dd($this->locations);
+//        dd($this->locations);
+//    }
 
     // the main function that des the calculations
-    public function compute(){
+    public function compute($shipment){
 
         $locations = $this->locations;
+//        dd($locations);
+//        array_unshift($perm, $shipment->name);
+//        array_pop($locations);
+//        $elevator = array_pop($locations);
 
         foreach ($locations as $location=>$coords){
+
             $this->longitudes[$location] = $coords['longitude'];
             $this->latitudes[$location] = $coords['latitude'];
         }
+
         $locations = array_keys($locations);
 
-        $this->all_routes = $this->array_permutations($locations);
+//        dd($locations);
+        $permutations = $this->array_permutations($locations, $perms = array());
+
+//        $shipment = Shipment::find($id_shipment);
+
+        foreach ($permutations as $perm){
+//            array_unshift($perm, $shipment->name);
+//            array_push($perm, $elevator);
+
+            $this->all_routes[] = $perm;
+        }
+
+        print_r($this->all_routes);
 
         foreach ($this->all_routes as $key=>$perms){
+//            dd($this->all_routes);
             $i=0;
             $total = 0;
+//            dd($this->latitudes);
             foreach ($perms as $value){
+//                dd($perms);
+//                dd($this->latitudes[$value]);
                 if ($i<count($this->locations)-1){
+//                    dd($this->latitudes);
+//                    dd($perms[0]);
+//                    dd($this->latitudes[$perms[$i]]);
                     $total+=$this->distance($this->latitudes[$perms[$i]],$this->longitudes[$perms[$i]],$this->latitudes[$perms[$i+1]],$this->longitudes[$perms[$i+1]]);
+//                    dd(111);
                 }
                 $i++;
             }
@@ -73,8 +101,7 @@ class TspController extends Controller
     }
 
     // work out all the possible different permutations of an array of data
-    private function array_permutations($items, $perms = array( )) {
-
+    private function array_permutations($items, $perms = array()) {
         static $all_permutations;
 
         if (empty($items)) {
@@ -89,6 +116,62 @@ class TspController extends Controller
             }
         }
         return $all_permutations;
+
+    }
+
+    function cartesian($input) {
+        $result = array();
+
+        while (list($key, $values) = each($input)) {
+            // If a sub-array is empty, it doesn't affect the cartesian product
+            if (empty($values)) {
+                continue;
+            }
+
+            // Seeding the product array with the values from the first sub-array
+            if (empty($result)) {
+                foreach($values as $value) {
+                    $result[] = array($key => $value);
+                }
+            }
+            else {
+                // Second and subsequent input sub-arrays work like this:
+                //   1. In each existing array inside $product, add an item with
+                //      key == $key and value == first item in input sub-array
+                //   2. Then, for each remaining item in current input sub-array,
+                //      add a copy of each existing array inside $product with
+                //      key == $key and value == first item of input sub-array
+
+                // Store all items to be added to $product here; adding them
+                // inside the foreach will result in an infinite loop
+                $append = array();
+
+                foreach($result as &$product) {
+                    // Do step 1 above. array_shift is not the most efficient, but
+                    // it allows us to iterate over the rest of the items with a
+                    // simple foreach, making the code short and easy to read.
+                    $product[$key] = array_shift($values);
+
+                    // $product is by reference (that's why the key we added above
+                    // will appear in the end result), so make a copy of it here
+                    $copy = $product;
+
+                    // Do step 2 above.
+                    foreach($values as $item) {
+                        $copy[$key] = $item;
+                        $append[] = $copy;
+                    }
+
+                    // Undo the side effecst of array_shift
+                    array_unshift($values, $product[$key]);
+                }
+
+                // Out of the foreach, we can add to $results now
+                $result = array_merge($result, $append);
+            }
+        }
+
+        return $result;
     }
 
     // return an array of the shortest possible route
@@ -115,18 +198,16 @@ class TspController extends Controller
 
         $shipment = Shipment::find($request->shipment);
 
-        $points['shipment'] = [
-            'name' => $shipment->name,
-            'latitude' => $shipment->latitude,
-            'longitude' => $shipment->longitude
-        ];
 
         foreach($request->tools as $tool){
 
-            $tools = Tool::where('name', $tool)->get();
+            $tools = Tool::where('name', $tool)->get()->all();
+
+
+//            dd($tools);
 
             foreach($tools as $tool){
-                $points[$tool->factory->name] = [
+                $points[$tool->name][] = [
                     'name' => $tool->factory->name,
                     'latitude' => $tool->factory->latitude,
                     'longitude' => $tool->factory->longitude
@@ -136,49 +217,118 @@ class TspController extends Controller
 
         $elevators = Elevator::all();
 
-        $closestElevator = $this->closestElevator($elevators, $points);
+//        $closestElevator = $this->closestElevator($elevators, $points);
 
-        $points['elevator'] = [
-            'name' => $closestElevator->name,
-            'latitude' => $closestElevator->latitude,
-            'longitude' => $closestElevator->longitude
-        ];
+        foreach($elevators as $elevator){
+            $points['elevator'][] = [
+                'name' => $elevator->name,
+                'latitude' => $elevator->latitude,
+                'longitude' => $elevator->longitude
+            ];
+        }
+
+        //Получаем все фабрики на которых есть этот Tool
 
         print_r($points);
 
-        $tsp = new TspController;
+        $result = $this->cartesian($points);
 
-        foreach($points as $point){
-            $tsp->add($point['name'], $point['longitude'], $point['latitude']);
+        print_r($result);
+
+        foreach ($result as $item){
+            dd($result);
+            $item[] = array_map("unserialize", array_unique( array_map("serialize", $item) ));
         }
 
-        $tsp->compute();
 
+
+        dd($item);
+//        print_r($final_points_array);
+
+        $tsp = new TspController;
+
+//        foreach ($final_points_array as $fpoints1){
+//            $fpoints1[] = [
+//                'name' => $shipment->name,
+//                'latitude' => $shipment->latitude,
+//                'longitude' => $shipment->longitude
+//            ];
+
+
+//            array_unshift($fpoints1, $fpoints1['shipment'] = [
+//                'name' => $shipment->name,
+//                'latitude' => $shipment->latitude,
+//                'longitude' => $shipment->longitude
+//            ]);
+
+//            $age = array_merge( [
+//                'shipment' => [
+//                    'name' => $shipment->name,
+//                    'latitude' => $shipment->latitude,
+//                    'longitude' => $shipment->longitude
+//                ]
+//            ], $fpoints1);
+//
+//            dd($age);
+
+//            array_unshift($fpoints1, [
+//                'name' => $shipment->name,
+//                'latitude' => $shipment->latitude,
+//                'longitude' => $shipment->longitude
+//            ]);
+//            dd($fpoints1);
+//        }
+
+
+
+//        $final_points_array = [
+//            'name' => $shipment->name,
+//            'latitude' => $shipment->latitude,
+//            'longitude' => $shipment->longitude
+//        ];
+
+//        dd($final_points_array);
+        foreach($final_points_array as $fpoints){
+            //            dd($fpoints);
+            foreach ($fpoints as $fpoint){
+
+                print_r($fpoints);
+                //$tsp->add($fpoint['name'], $fpoint['longitude'], $fpoint['latitude']);
+                $this->locations[$fpoint['name']] = array('longitude'=>$fpoint['longitude'],'latitude' => $fpoint['latitude']);
+            }
+        }
+//        dd(count($this->locations));
+        dd($this->locations);
+
+
+
+        $tsp->compute($shipment);
+//        dd($this->locations);
 
         echo 'Shortest Distance: '.$tsp->shortest_distance();
 
-        echo '<br />Shortest Route: ';
+        echo '<br> Shortest Route: ';
 
         print_r($tsp->shortest_route());
 
-        echo '<br />Num Routes: '.count($tsp->routes());
+        echo '<br> Num Routes: '.count($tsp->routes());
 
-        echo '<br />Matching shortest Routes: ';
+        echo '<br> Matching shortest Routes: ';
 
         print_r($tsp->matching_shortest_routes());
 
-        echo '<br />All Routes: ';
+        echo '<br>All Routes: ';
 
         print_r($tsp->routes());
     }
 
-    public function closestElevator($elevators, $points){
-        foreach ($elevators as $elevator){
-            $distance = $this->distance($points['shipment']['latitude'], $points['shipment']['longitude'], $elevator->latitude, $elevator->longitude);
-            $elevatorPoint[][$elevator->id] = $distance;
-        }
-
-        $elevator = Elevator::find(key(min($elevatorPoint)));
-        return $elevator;
-    }
+//    public function closestElevator($elevators, $points){
+//        foreach ($elevators as $elevator){
+//            $distance = $this->distance($points['shipment']['latitude'], $points['shipment']['longitude'], $elevator->latitude, $elevator->longitude);
+//            $elevatorPoint[][$elevator->id] = $distance;
+//        }
+//
+//        $elevator = Elevator::find(key(min($elevatorPoint)));
+//        return $elevator;
+//    }
 }
